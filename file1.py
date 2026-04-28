5098482f1765f48df2da4c586e5586d55373a487
Supriya <sanisupriya9@gmail.com>
2026-04-28 12:57:32 +0530

    version 1 of image editing

--- file1.py
+++ file1.py
@@ -1,7 +1,17 @@
 import streamlit as st
+import cv2
 st.title("Machine Learning")
 st.write("Hello World")
-
 st.write("Deep Learning")
+#a=st.number_input("Enter a number")
+#st.text(a)
+#st.image("Imagepre.png")
 
-st.image("Imagepre.png")
+img=cv2.imread("Imagepre.png")
+
+st.image(img)
+st.write("Resize it")
+height=st.slider("select the height",100,500)
+width=st.slider("select the width",100,500)
+img1=cv2.resize(img,(width,height))
+st.image(img1)
