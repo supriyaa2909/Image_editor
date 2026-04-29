fe29db990a39c6a3c3ef940f2db7d48308b1da54
Supriya <sanisupriya9@gmail.com>
2026-04-29 11:28:29 +0530

    page2

--- photos.py
+++ photos.py
@@ -6,4 +6,4 @@ if "uploaded_file" in st.session_state:
     st.image(st.session_state["uploaded_file"])
     st.write("Now you can edit your image!")
 else:
-    st.warning("Please upload an image first.")import streamlit as stst.title("Image Editor")if "uploaded_file" in st.session_state:    st.image(st.session_state["uploaded_file"])    st.write("Now you can edit your image!")else:    st.warning("Please upload an image first.")
+    st.warning("Please upload an image first.")
